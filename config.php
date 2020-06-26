b57dbdfe0cb6823b2860e436a0824d1c7e307814
SS-2020 <63364168+SS-2020@users.noreply.github.com>
2020-06-26 12:38:30 +0530

    added create table commands as comment

--- config.php
+++ config.php
@@ -12,4 +12,34 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+/*
+ 	CREATE TABLE `users` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `username` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+ `password` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+ `created_at` datetime DEFAULT current_timestamp(),
+ PRIMARY KEY (`id`),
+ UNIQUE KEY `username` (`username`)
+) 
+CREATE TABLE `eventtable` (
+ `no` int(11) NOT NULL AUTO_INCREMENT,
+ `uid` int(11) NOT NULL,
+ `uname` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+ `type` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+ `header` text COLLATE utf8mb4_unicode_ci NOT NULL,
+ `venue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+ `date` date NOT NULL,
+ `time` time NOT NULL,
+ `footer` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+ PRIMARY KEY (`no`)
+)
+ CREATE TABLE `invite` (
+ `eno` int(11) DEFAULT NULL,
+ `userid` int(11) DEFAULT NULL,
+ `name` varchar(25) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+ `food` varchar(6) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+ `people` varchar(5) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+ `status` varchar(7) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+)
+ */
 ?>
