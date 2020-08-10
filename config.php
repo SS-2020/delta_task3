eaf82f59faec4f97c309e74a65ccdc4025c73750
SS-2020 <63364168+SS-2020@users.noreply.github.com>
2020-08-11 00:29:08 +0530

    included db commands

--- config.php
+++ config.php
@@ -23,8 +23,8 @@ if($link === false){
 
 $sql1="CREATE TABLE IF NOT EXISTS `users` (
  `id` int(11) NOT NULL AUTO_INCREMENT,
- `username` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
- `password` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+ `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+ `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
  `created_at` datetime DEFAULT current_timestamp(),
  PRIMARY KEY (`id`),
  UNIQUE KEY `username` (`username`)
