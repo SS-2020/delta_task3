a42ab18bee558514f511734d201ec9a2211e9fdf
SS-2020 <63364168+SS-2020@users.noreply.github.com>
2020-08-11 12:29:33 +0530

    undid prev commit

--- config.php
+++ config.php
@@ -4,15 +4,7 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'userdetails');
-$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
-if($conn === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-$query="CREATE DATABASE IF NOT EXISTS userdetails";
-if (mysqli_query($conn, $query));
-else {
-  echo "Error creating database: " . mysqli_error($conn);
-}
+ 
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
@@ -21,19 +13,16 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-$sql1="CREATE TABLE IF NOT EXISTS `users` (
+/*
+ 	CREATE TABLE `users` (
  `id` int(11) NOT NULL AUTO_INCREMENT,
- `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
- `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+ `username` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+ `password` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
  `created_at` datetime DEFAULT current_timestamp(),
  PRIMARY KEY (`id`),
  UNIQUE KEY `username` (`username`)
-)"; 
-if (mysqli_query($link, $sql1));
-else {
-  echo "Error creating table: " . mysqli_error($link);
-}
-$sql2="CREATE TABLE IF NOT EXISTS `eventtable` (
+) 
+CREATE TABLE `eventtable` (
  `no` int(11) NOT NULL AUTO_INCREMENT,
  `uid` int(11) NOT NULL,
  `uname` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -44,21 +33,14 @@ $sql2="CREATE TABLE IF NOT EXISTS `eventtable` (
  `time` time NOT NULL,
  `footer` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
  PRIMARY KEY (`no`)
-)";
-if (mysqli_query($link, $sql2));
-else {
-  echo "Error creating table: " . mysqli_error($link);
-}
-$sql3="CREATE TABLE IF NOT EXISTS`invite` (
+)
+ CREATE TABLE `invite` (
  `eno` int(11) DEFAULT NULL,
  `userid` int(11) DEFAULT NULL,
  `name` varchar(25) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
  `food` varchar(6) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
  `people` varchar(5) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
  `status` varchar(7) COLLATE utf8mb4_unicode_ci DEFAULT NULL
-)";
-if (mysqli_query($link, $sql3));
-else {
-  echo "Error creating table: " . mysqli_error($link);
-}
+)
+ */
 ?>
